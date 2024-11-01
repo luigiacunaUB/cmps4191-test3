77f5975541f00ead255e23e21c62b4213065f807
Luigi Acuna <2015112896@ub.edu.bz>
2024-11-01 01:54:45 -0400

    validation done

--- cmd/api/product.go
+++ cmd/api/product.go
@@ -3,16 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/luigiacunaUB/cmps4191-test3/internal/data"
+	"github.com/luigiacunaUB/cmps4191-test3/internal/validator"
 )
 
 func (a *applicationDependencies) createProduct(w http.ResponseWriter, r *http.Request) {
 	var incomingData struct {
-		ProductName string `json:"productname`
+		ProdName string `json:"productname"`
 	}
 
 	err := a.readJSON(w, r, &incomingData)
 	if err != nil {
 		a.badRequestResponse(w, r, err)
+		return
+	}
+	product := &data.Product{
+		ProdName: incomingData.ProdName,
+	}
+
+	v := validator.New()
+
+	data.ValidateProduct(v, product)
+	if !v.IsEmpty() {
+		a.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
--- internal/data/products.go
+++ internal/data/products.go
@@ -2,10 +2,17 @@ package data
 
 import (
 	"time"
+
+	"github.com/luigiacunaUB/cmps4191-test3/internal/validator"
 )
 
 type Product struct {
-	ID        int64
-	prodName  string
-	addedDate time.Time
+	ID        int64     `json:"id"`
+	ProdName  string    `json:"productname"`
+	AddedDate time.Time `json:"-"`
+}
+
+func ValidateProduct(v *validator.Validator, product *Product) {
+	v.Check(product.ProdName != "", "productname", "must be provided")
+	v.Check(len(product.ProdName) <= 25, "productname", "must not be more than 25 bytes long")
 }
